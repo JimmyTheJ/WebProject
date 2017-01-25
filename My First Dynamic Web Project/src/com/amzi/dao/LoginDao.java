8db7c95798a3b86309e7eac1f082d9f84ca7e176
Cameron <cameronriice@gmail.com>
2017-01-25 16:23:50 -0500

    Test Commit

--- My First Dynamic Web Project/src/com/amzi/dao/LoginDao.java
+++ My First Dynamic Web Project/src/com/amzi/dao/LoginDao.java
@@ -17,7 +17,7 @@ public class LoginDao {
         String dbName = "form";
         String driver = "com.mysql.jdbc.Driver";
         String userName = "root";
-        String password = "i+g7fvJRXNesQZb^";
+        String password = "yA720672";
         try {
             Class.forName(driver).newInstance();
             conn = DriverManager
